3fef329a5e1a682e70e1822b563b013686516568
kamalbuilds <geniusamansingh@gmail.com>
2025-08-17 10:10:52 -0400

    feat added: opensea listing

--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,23 @@
 import crypto from 'crypto';
 import { flaunchActionProvider } from '@coinbase/agentkit';
+import { baseMcpOpenseaActionProvider } from './tools/opensea/index.js';
 
 /**
  * Some AgentKit action providers throw if a key isn't set
  * This function returns a list of action providers that have required env vars
  */
 export function getActionProvidersWithRequiredEnvVars() {
+  const providers = [];
+  
   if (process.env.PINATA_JWT) {
-    return [flaunchActionProvider()];
+    providers.push(flaunchActionProvider());
+  }
+  
+  if (process.env.OPENSEA_API_KEY) {
+    providers.push(baseMcpOpenseaActionProvider());
   }
 
-  return [];
+  return providers;
 }
 
 export function generateSessionId(): string {
